ba6edb9857eba5fb0d0817aa208782cd79d25631
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2024-02-22 23:58:46 +0300

    Пока играем только на одном компе, сетевые - косячные

--- Checkers/CheckersApp/Program.cs
+++ Checkers/CheckersApp/Program.cs
@@ -16,7 +16,7 @@ namespace CheckersApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RussianNetworkCheckersForm());
+            Application.Run(new SelectingForm());
         }
     }
 }
